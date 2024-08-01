eb21c57db78f5ef447dfaf79ae8bbf2b939136fc
bsukalo <bsukalo@outlook.com>
2024-08-01 18:23:44 +0200

    Import axios

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,17 +1,23 @@
-import { FormEvent, useRef } from "react";
+import axios from "axios";
+import { FormEvent, useState } from "react";
 
 const LoginForm = () => {
-	const userRef = useRef<HTMLInputElement>(null);
-	const passRef = useRef<HTMLInputElement>(null);
+	const [username, setUsername] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
 
 	const handleSubmit = (event: FormEvent) => {
 		event.preventDefault();
-		if (userRef.current !== null) {
-			console.log(userRef.current.value);
-		}
-		if (passRef.current !== null) {
-			console.log(passRef.current.value);
-		}
+		axios
+			.post("http://127.0.0.1:3000/api/login", {
+				username,
+				password,
+			})
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((error) => {
+				console.log(error.message);
+			});
 	};
 
 	return (
@@ -20,19 +26,19 @@ const LoginForm = () => {
 				className="d-flex flex-column justify-content-center align-items-center"
 				style={{ minHeight: "90vh" }}
 			>
-				<div className="mb-4 text-center">
+				<div className="mb-3 text-center">
 					<h1>
 						MedicLab
 						<br />
 						Login
 					</h1>
 				</div>
-				<div className="mb-4">
+				<div className="mb-3">
 					<label htmlFor="username" className="visually-hidden">
 						Username
 					</label>
 					<input
-						ref={userRef}
+						onChange={(event) => setUsername(event.target.value)}
 						id="username"
 						type="text"
 						autoComplete="username"
@@ -40,19 +46,19 @@ const LoginForm = () => {
 						placeholder="Username"
 					/>
 				</div>
-				<div className="mb-4">
+				<div className="mb-3">
 					<label htmlFor="password" className="visually-hidden">
 						Password
 					</label>
 					<input
-						ref={passRef}
+						onChange={(event) => setPassword(event.target.value)}
 						id="password"
 						type="password"
 						className="form-control"
 						placeholder="Password"
 					/>
 				</div>
-				<button className="btn btn-primary mt-3">Login</button>
+				<button className="btn btn-primary">Login</button>
 			</div>
 		</form>
 	);
