085e3c947d26d9a57946910a37a818c6c616de25
bsukalo <bsukalo@outlook.com>
2024-08-03 23:55:25 +0200

    Create modal template

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import apiClient from "../services/api-client";
+import Modal from "./Modal";
+import { useState } from "react";
+import RegistrationForm from "./RegistrationForm";
 
 const HomeHeader = () => {
+	const [showModal, setShowModal] = useState(false);
 	const navigate = useNavigate();
 
 	const handleLogOut = () => {
@@ -19,25 +23,46 @@ const HomeHeader = () => {
 	};
 
 	return (
-		<div
-			className="px-4 position-fixed d-flex flex-row justify-content-between align-items-center gap-3"
-			style={{
-				backgroundColor: "rgb(14,17,21)",
-				height: "100px",
-				top: 0,
-				left: 0,
-				right: 0,
-				zIndex: 1,
-			}}
-		>
-			<h2 className="my-1">MedicLab Home</h2>
-			<div className="d-flex justify-content-between gap-3">
-				<div className="btn btn-primary">New user</div>
-				<div className="btn btn-outline-light" onClick={handleLogOut}>
-					Log out
+		<>
+			<Modal
+				show={showModal}
+				onClose={() => {
+					setShowModal(false);
+				}}
+				title={"Register new user"}
+			>
+				<RegistrationForm />
+			</Modal>
+			<div
+				className="px-4 position-fixed d-flex flex-row justify-content-between align-items-center gap-3"
+				style={{
+					backgroundColor: "rgb(14,17,21)",
+					height: "100px",
+					top: 0,
+					left: 0,
+					right: 0,
+					zIndex: 1,
+				}}
+			>
+				<h2 className="my-1">MedicLab Home</h2>
+				<div className="d-flex justify-content-between gap-3">
+					<div
+						className="btn btn-primary"
+						onClick={() => {
+							setShowModal(true);
+						}}
+					>
+						New user
+					</div>
+					<div
+						className="btn btn-outline-light"
+						onClick={handleLogOut}
+					>
+						Log out
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
---
+++ src/components/Modal.tsx
@@ -0,0 +1,60 @@
+import { FC, ReactElement } from "react";
+import { RiCloseLargeFill } from "react-icons/ri";
+
+interface ModalProps {
+	show: boolean;
+	onClose: () => void;
+	title: string;
+	children: ReactElement;
+}
+
+export default function Modal(props: ModalProps): ReturnType<FC> {
+	return (
+		<div
+			className={`${"modal"} ${props.show ? "d-block" : "d-none"}`}
+			style={{
+				position: "fixed",
+				top: 0,
+				left: 0,
+				width: "100%",
+				height: "100%",
+				background: "rgba(0,0,0,0.5)",
+			}}
+		>
+			<div className="d-flex justify-content-center align-items-center">
+				<span
+					className="card"
+					style={{
+						top: 50,
+						bottom: 100,
+						width: "80%",
+						height: "auto",
+						marginBlockEnd: "100px",
+					}}
+				>
+					<div
+						className="d-flex align-items-top justify-content-between"
+						style={{ height: "100px" }}
+					>
+						<h2
+							style={{
+								padding: "20px 0px 00px 20px",
+								maxWidth: "300px",
+							}}
+						>
+							{props.title}
+						</h2>
+						<button
+							className="btn btn-outline-danger d-flex align-items-center justify-content-center"
+							style={{ height: "40px", width: "40px" }}
+							onClick={props.onClose}
+						>
+							<RiCloseLargeFill size={20} />
+						</button>
+					</div>
+					<div>{props.children}</div>
+				</span>
+			</div>
+		</div>
+	);
+}
