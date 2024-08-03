3ee5710d06591d3c07c81bc31766699286de70b7
bsukalo <bsukalo@outlook.com>
2024-08-03 18:14:27 +0200

    Update API client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,12 +1,24 @@
 import axios from "axios";
 
-const token = localStorage.getItem("login_token");
-
-export default axios.create({
+const apiClient = axios.create({
 	baseURL: "http://127.0.0.1:3000/api",
 	withCredentials: true,
 	headers: {
-		Authorization: `Bearer ${token}`,
 		"Content-Type": "application/json",
 	},
 });
+
+apiClient.interceptors.request.use(
+	(config) => {
+		const token = localStorage.getItem("login_token");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+export default apiClient;
